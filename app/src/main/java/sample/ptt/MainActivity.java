8823d789ec7cdf341c7e3d2821c78e133c3cec3f
FihlaTV <fihlatv@gmail.com>
2021-05-02 09:45:59 +0200

    Upgraded to sdk 29 Pushed PTT logo

--- app/src/main/java/sample/ptt/MainActivity.java
+++ app/src/main/java/sample/ptt/MainActivity.java
@@ -2,8 +2,8 @@ package sample.ptt;
 
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.webkit.CookieManager;
 import android.webkit.WebSettings;
--- app/src/main/java/sample/ptt/MainActivity2.java
+++ app/src/main/java/sample/ptt/MainActivity2.java
@@ -1,6 +1,6 @@
 package sample.ptt;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
